dc3de57154b044e8a4b66a89e18e74290661640b
Himanshu Hemant <93419218+himanshu0sinha@users.noreply.github.com>
2024-04-25 21:08:48 +0530

    Update install_jenkins.sh

--- jenkins-tf/install_jenkins.sh
+++ jenkins-tf/install_jenkins.sh
@@ -23,9 +23,3 @@ newgrp docker
 sudo chmod 777 /var/run/docker.sock
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
-# install trivy
-sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update
-sudo apt-get install trivy -y
